1518563d4f7bcb0ca93c770ed2fa3cdf409e70f4
falcon <falcon_chen@qq.com>
2020-01-05 21:04:34 +0800

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Falcon on 2020/1/5.
+//  Copyright © 2020 Falcon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
